4306447f852e172753784097d58e4e4d33d4c0a1
ahmad-cod <ahmadadewale800@gmail.com>
2025-04-24 21:49:46 +0100

    Add image configuration to next.config.ts: define domains and remote patterns

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["res.cloudinary.com", "images.unsplash.com", "cdn.pixabay.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+  },]
+}
 };
 
 export default nextConfig;
