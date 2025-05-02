78942340d40bc2e93161df334f807b3fc2a5e841
ahmad-cod <ahmadadewale800@gmail.com>
2025-05-02 22:52:41 +0100

    Refactor Home component layout: remove async keyword and clean up JSX structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,23 @@
+import CTA from "@/components/cta";
 import Hero from "@/components/hero";
 import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
 import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import { FeedbackCard } from "@/components/feedback-card";
+import RecentFeedbacks from "@/components/recent-feedbacks";
 
-export default async function Home() {
+export default function Home() {
   return (
-    <>
+    <section className="flex flex-col gap-10">
       <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
+      {/* <main className="flex-1 flex flex-col gap-6">
         <h2 className="font-medium text-xl mb-4">Next steps</h2>
         {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
-    </>
+
+      </main> */}
+
+      <RecentFeedbacks />
+      <CTA />
+    </section>
   );
 }
