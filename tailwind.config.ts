ed80de9a402173c95d800b2275dc12a4851c559a
ahmad-cod <ahmadadewale800@gmail.com>
2025-05-05 23:27:34 +0100

    feat: add custom font families for heading and body in Tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,10 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        heading: ["var(--font-poppins)", "sans-serif"],
+        body: ["var(--font-roboto)", "sans-serif"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
