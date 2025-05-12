d39acf4f3e9a639d78471b4ae4b6dfee73952185
ahmad-cod <ahmadadewale800@gmail.com>
2025-05-13 00:38:47 +0100

    refactor:

--- components/footer.tsx
+++ components/footer.tsx
@@ -2,46 +2,17 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="bg-gray-100 border-t border-gray-300 py-6 sm:py-12 text-center">
-      <div className="flex justify-between items-center max-w-5xl mx-auto px-4">
-      <p className="text-gray-600 py-6">
-          &copy; {new Date().getFullYear()} SUGBUK. All rights reserved.
-        </p>
-        <ul className="flex justify-center space-x-6">
-          <li>
-            <a href="/about" className="text-blue-500 hover:underline">
-              About
-            </a>
-          </li>
-          <li>
-            <a href="/contact" className="text-blue-500 hover:underline">
-              Contact
-            </a>
-          </li>
-          <li>
-            <a href="/privacy" className="text-blue-500 hover:underline">
-              Privacy Policy
-            </a>
-          </li>
-        </ul>
-        <p className="text-gray-600">
-          New Site, Gwarzo Road, Kano
-        </p>
+    <footer className="bg-[#212529] text-white py-5 text-center">
+      <div className="flex flex-col md:flex-row justify-center items-center gap-2 md:gap-4 text-sm">
+        <span>© 2025 SUG-BUK</span>
+        <span className="hidden md:inline">•</span>
+        <span>Your Voice Matters at Bayero University, Kano</span>
+        <span className="hidden md:inline">•</span>
+        <span>
+          Built by{' '}
+          <span className="">Ahmad Aroyehun</span>
+        </span>
       </div>
-      
-
-        {/* <p className="text-gray-600 mb-4">
-          Built with ❤️ by 
-          <strong>
-          <a
-            href="https://ahmadaroyehun.netlify.app"
-            target="_blank"
-            className="font-bold hover:underline"
-            rel="noreferrer"
-          >
-            Ahmad Aroyehun
-          </a></strong>
-        </p> */}
     </footer>
   );
 };
