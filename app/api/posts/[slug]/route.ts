db793cfbc1585dbf30765cc0793e094b958e572b
ahmad-cod <ahmadadewale800@gmail.com>
2025-05-14 17:06:47 +0100

    fix: params returns a promise

--- app/api/posts/[slug]/route.ts
+++ app/api/posts/[slug]/route.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@/utils/supabase/client'
 import { NextRequest, NextResponse } from 'next/server'
 
-export async function GET(request: NextRequest, { params }: { params: { slug: string } }) {
-  const { slug } = params
+export async function GET(request: NextRequest, { params }: { params: Promise<{ slug: string }>  }) {
+  const { slug } = await params
   if (!slug) {
     return NextResponse.json({ error: 'Slug is required' }, { status: 400 })
   }
