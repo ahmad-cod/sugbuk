cf0e1cbab3bd523438fec06e0f799bfc483d2774
ahmad-cod <ahmadadewale800@gmail.com>
2025-05-15 17:44:52 +0100

    fix: dynamically import RecentFeedbacks component for improved loading experience

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+"use client";
+import dynamic from "next/dynamic";
 import CTA from "@/components/cta";
 import Hero from "@/components/hero";
-import RecentFeedbacks from "@/components/recent-feedbacks";
 
+const RecentFeedbacks = dynamic(() => import('@/components/recent-feedbacks'), {
+  ssr: false,
+  loading: () => <div className="h-96 w-full flex items-center justify-center">Loading...</div>,
+});
 export default function Home() {
   return (
     <section className="flex flex-col gap-20">
