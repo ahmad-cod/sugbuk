5d7ae3bc0988a06830c23020bc85e4ba73fdea5e
ahmad-cod <ahmadadewale800@gmail.com>
2025-05-23 15:36:46 +0100

    fix: update team members images and restore commented entries

--- constants/team-members.ts
+++ constants/team-members.ts
@@ -12,7 +12,7 @@ export const TEAM_MEMBERS = [
     name: "Amina Yusuf",
     position: "Vice President",
     faculty: "Faculty of Social Sciences",
-    image: "/images/dev_avatar.jpg",
+    image: "/images/president.jpg",
     contact: "vp@sugbuk.org"
   },
   {
@@ -20,79 +20,80 @@ export const TEAM_MEMBERS = [
     name: "Fatima Ahmad",
     position: "General Secretary",
     faculty: "Faculty of Law",
-    image: "/images/dev_avatar.jpg",
+    image: "/images/pro.jpg",
     contact: "secretary@sugbuk.org"
   },
-  {
+    {
     id: 4,
-    name: "Mohammed Ali",
-    position: "Financial Secretary",
-    faculty: "Faculty of Management Sciences",
-    image: "/images/dev_avatar.jpg",
-    contact: "finance@sugbuk.org"
-  },
-  {
-    id: 5,
-    name: "Ibrahim Hassan",
+    name: "Abubakar Salisu Kazaure",
     position: "Public Relations Officer",
-    faculty: "Faculty of Communication",
-    image: "/images/dev_avatar.jpg",
+    faculty: "Bsc; (Anatomy in view)",
+    image: "/images/pro.jpg",
     contact: "pro@sugbuk.org"
   },
-  {
-    id: 6,
-    name: "Zainab Mohammed",
-    position: "Welfare Secretary",
-    faculty: "Faculty of Medicine",
-    image: "/images/dev_avatar.jpg",
-    contact: "welfare@sugbuk.org"
-  },
-  {
-    id: 7,
-    name: "Umar Abdullahi",
-    position: "Sports Secretary",
-    faculty: "Faculty of Science",
-    image: "/images/dev_avatar.jpg",
-    contact: "sports@sugbuk.org"
-  },
-  {
-    id: 8,
-    name: "Aisha Suleiman",
-    position: "Academic Secretary",
-    faculty: "Faculty of Education",
-    image: "/images/dev_avatar.jpg",
-    contact: "academic@sugbuk.org"
-  },
-  {
-    id: 9,
-    name: "Yusuf Ibrahim",
-    position: "Director of Socials",
-    faculty: "Faculty of Arts",
-    image: "/images/dev_avatar.jpg",
-    contact: "socials@sugbuk.org"
-  },
-  {
-    id: 10,
-    name: "Halima Abubakar",
-    position: "Treasurer",
-    faculty: "Faculty of Economics",
-    image: "/images/dev_avatar.jpg",
-    contact: "treasurer@sugbuk.org"
-  },
-  {
-    id: 11,
-    name: "Musa Aliyu",
-    position: "Assistant General Secretary",
-    faculty: "Faculty of Computer Science",
-    image: "/images/dev_avatar.jpg",
-    contact: "asstsecretary@sugbuk.org"
-  },
-  {
-    id: 12,
-    name: "Hauwa Garba",
-    position: "Director of Information",
-    faculty: "Faculty of Environmental Sciences",
-    image: "/images/dev_avatar.jpg",
-    contact: "information@sugbuk.org"
-  }
+  // {
+  //   id: 5,
+  //   name: "Mohammed Ali",
+  //   position: "Financial Secretary",
+  //   faculty: "Faculty of Management Sciences",
+  //   image: "/images/dev_avatar.jpg",
+  //   contact: "finance@sugbuk.org"
+  // },
+
+  // {
+  //   id: 6,
+  //   name: "Zainab Mohammed",
+  //   position: "Welfare Secretary",
+  //   faculty: "Faculty of Medicine",
+  //   image: "/images/dev_avatar.jpg",
+  //   contact: "welfare@sugbuk.org"
+  // },
+  // {
+  //   id: 7,
+  //   name: "Umar Abdullahi",
+  //   position: "Sports Secretary",
+  //   faculty: "Faculty of Science",
+  //   image: "/images/dev_avatar.jpg",
+  //   contact: "sports@sugbuk.org"
+  // },
+  // {
+  //   id: 8,
+  //   name: "Aisha Suleiman",
+  //   position: "Academic Secretary",
+  //   faculty: "Faculty of Education",
+  //   image: "/images/dev_avatar.jpg",
+  //   contact: "academic@sugbuk.org"
+  // },
+  // {
+  //   id: 9,
+  //   name: "Yusuf Ibrahim",
+  //   position: "Director of Socials",
+  //   faculty: "Faculty of Arts",
+  //   image: "/images/dev_avatar.jpg",
+  //   contact: "socials@sugbuk.org"
+  // },
+  // {
+  //   id: 10,
+  //   name: "Halima Abubakar",
+  //   position: "Treasurer",
+  //   faculty: "Faculty of Economics",
+  //   image: "/images/dev_avatar.jpg",
+  //   contact: "treasurer@sugbuk.org"
+  // },
+  // {
+  //   id: 11,
+  //   name: "Musa Aliyu",
+  //   position: "Assistant General Secretary",
+  //   faculty: "Faculty of Computer Science",
+  //   image: "/images/dev_avatar.jpg",
+  //   contact: "asstsecretary@sugbuk.org"
+  // },
+  // {
+  //   id: 12,
+  //   name: "Hauwa Garba",
+  //   position: "Director of Information",
+  //   faculty: "Faculty of Environmental Sciences",
+  //   image: "/images/dev_avatar.jpg",
+  //   contact: "information@sugbuk.org"
+  // }
 ];
