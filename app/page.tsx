cb350e6fb390e52cb59b0de518848d0851bb9dba
ahmad-cod <ahmadadewale800@gmail.com>
2025-10-20 09:47:24 +0100

    refactor: remove unused recent feedback component

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import NewsCarousel from "@/components/carousel/news-carousel";
 import CTA from "@/components/cta";
 import Hero from "@/components/hero";
 import MeetTheTeam from "@/components/meet-the-team";
-import RecentFeedbacks from "@/components/recent-feedbacks";
 import { TEAM_MEMBERS } from "@/constants/team-members";
 
 export default function Home() {
